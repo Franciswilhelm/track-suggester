262e3b6405a545ed0c16432cd569e80e75565a78
Francis Prus <francis.prus@hotmail.com>
2016-08-12 14:51:14 -0700

    introduce working js code with results 'C+,' 'Php,' and 'Ruby'.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,15 @@ var casual = "Ruby";
 $(document).ready(function() {
   $("form#survey").submit(function() {
     event.preventDefault();
-
-    
+    var clothes = $("input:radio[name=clothes]:checked").val();
+    var result;
+    if (clothes === "business") {
+      result = "C+";
+    } else if (clothes === "businesscasual") {
+      result = "Php";
+    } else if (clothes === "casual") {
+      result = "Ruby";
+    }
     $("#output").text(result);
   });
 });
