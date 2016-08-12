4844f50adc587ccadbbfad2c5373680a6fb2da0b
Francis Prus <francis.prus@hotmail.com>
2016-08-12 15:25:31 -0700

    Make js logic more complex

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,15 @@ $(document).ready(function() {
   $("form#survey").submit(function() {
     event.preventDefault();
     var clothes = $("input:radio[name=clothes]:checked").val();
+    var company = $("input:radio[name=company]:checked").val();
+    var letter = $("input:radio[name=letter]:checked").val();
+    var device = $("input:radio[name=device]:checked").val();
     var result;
-    if (clothes === "business") {
+    if (clothes === "business" || company === "big") {
       result = "C+";
-    } else if (clothes === "businesscasual") {
+    } else if (clothes === "businesscasual" || company === "website") {
       result = "Php";
-    } else if (clothes === "casual") {
+    } else if (clothes === "casual" || company === "startup" && letter === "r" || device === "app" ) {
       result = "Ruby";
     }
     $("#output").text(result);
