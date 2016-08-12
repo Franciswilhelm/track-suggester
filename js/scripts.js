4cf43b5d92c9fc3d8ca3b8fd049eb10c3c8ab265
Francis Prus <francis.prus@hotmail.com>
2016-08-12 11:20:45 -0700

    Add js variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
+var biz = function() {return alert("C+")};
+var bizcasual = function() {return alert("PHP")};
+var casual = function() {return alert("Ruby")};
+
+
 $(document).ready(function() {
   $("form#survey").submit(function() {
     event.preventDefault();
-    var number1 = 
-
-
 
     $("#output").text(result);
   });
